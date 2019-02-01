c61f09d70937dce544f198eae52b2449ae7024e4
Shashwat Sinha <shashwat_sinha@external.mckinsey.com>
2019-02-01 12:19:49 +0530

    Getting response fromn both API and making then onject done

--- src/helpers/getAllBooks.js
+++ src/helpers/getAllBooks.js
@@ -1,43 +1,54 @@
 const axios = require('axios');
 
-const listOfBooks = [];
 const getListOfBooks = async () => {
   const result = await axios.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks');
   const bookObjects = result.data.books;
   return bookObjects;
 };
-// const getRatingOfBook = async (url) => {
-//   const posts = await axios.get(url);
-//   return posts.data;
-// };
-// const getRatingsOfBooks = (urls, ratingFunction) => {
-//   const ratings = [];
-//   const numberOfBooks = urls.length;
-//   for (let i = 0; i < numberOfBooks; i += 1) {
-//     const result = ratingFunction(urls[i]);
-//     result.then((bookRating) => {
-//       ratings[i] = bookRating;
-//     });
-//   }
+const getRatingOfBook = async (url) => {
+  const posts = await axios.get(url);
+  return posts.data;
+};
+const getRatingsOfBooks = (urls, ratingFunction) => {
+  const numberOfBooks = urls.length;
+  const promisesOfAxiosCall = [];
+  for (let i = 0; i < numberOfBooks; i += 1) {
+    promisesOfAxiosCall[i] = ratingFunction(urls[i]);
+  }
+  return Promise.all(promisesOfAxiosCall).then(ratingValue => ratingValue);
+};
+const ratingsUrl = (id) => {
+  const baseUrl = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/';
+  const url = baseUrl.concat(id);
+  return url;
+};
 
-//   return ratings;
-// };
-// const ratingsUrl = (id) => {
-//   const baseUrl = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/';
-//   const url = baseUrl.concat(id);
-//   return url;
-// };
+const getBookDetailsWithRatings = () => {
+  const result = getListOfBooks();
+  return result.then(async (bookObjects) => {
+  // This book object will contain ratings also
+    const newBookArray = [];
+    // Extracting id in separate array
+    const ids = bookObjects.map(bookObject => bookObject.id);
+    // Generating urls for all books
+    const listOfUrls = [];
+    ids.forEach(id => listOfUrls.push(ratingsUrl(id)));
+    // Making request for each urls
+    let bookRatings = [];
+    bookRatings = await getRatingsOfBooks(listOfUrls, getRatingOfBook);
+    const numberOfBooks = listOfUrls.length;
+    for (let i = 0; i < numberOfBooks; i += 1) {
+      const tempBookObject = {};
+      tempBookObject.id = bookObjects[i].id;
+      tempBookObject.Name = bookObjects[i].Name;
+      tempBookObject.Author = bookObjects[i].Author;
+      tempBookObject.rating = bookRatings[i].rating;
+      newBookArray.push(tempBookObject);
+    }
+    // console.log(newBookArray);
+    return newBookArray;
+  });
+};
+getBookDetailsWithRatings();
 
-const result = getListOfBooks();
-result.then(async bookObjects => bookObjects);
-// // Extracting id in separate array
-// const ids = bookObjects.map(bookObject => bookObject.id);
-// // Generating urls for all books
-// const listOfUrls = [];
-// ids.forEach(id => listOfUrls.push(ratingsUrl(id)));
-// // Making request for each urls
-// let bookRatings = [];
-// bookRatings = await getRatingsOfBooks(listOfUrls, getRatingOfBook);
-// console.log(bookRatings);
-
-module.exports = getListOfBooks;
+module.exports = getBookDetailsWithRatings;
--- tests/src/helpers/getAllBooks.test.js
+++ tests/src/helpers/getAllBooks.test.js
@@ -1,8 +1,8 @@
-const getListOfBooks = require('./../../../src/helpers/getAllBooks');
+const getBookDetailsWithRatings = require('./../../../src/helpers/getAllBooks');
 
 describe('getListOfBooks () :', () => {
   it('should respond with status code 200 when request is made to list of books at API 1', async () => {
-    const temp = await getListOfBooks();
+    const temp = await getBookDetailsWithRatings();
     console.log(temp);
     expect(temp).toEqual(200);
   });
